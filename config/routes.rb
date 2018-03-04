4959e0626d09655b2a461d1ea023e4470eda86bd
choongon <omomo99@naver.com>
2018-03-04 07:20:49 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  #create
+  get '/posts/new' => 'posts#new'
+  post '/posts/create' => 'posts#create'
+  #read
+  get '/' => 'posts#index'
+  get '/posts/show/:post_id' => 'posts#show'
+   # Update
+  get '/posts/edit/:post_id' => 'posts#edit'
+  post '/posts/update/:post_id' => 'posts#update'
+  #Delete
+  post '/posts/destroy/:post_id' => 'posts#destroy'
+ 
+end
